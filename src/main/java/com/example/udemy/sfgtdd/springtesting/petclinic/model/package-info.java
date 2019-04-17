3788ede3bedbb5018bb0579f72d70e065e149634
SergeA <ronnieo@gmail.com>
2019-04-17 13:42:26 -0400

    Initial Commit

---
+++ src/main/java/com/example/udemy/sfgtdd/springtesting/petclinic/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The classes in this package represent PetClinic's business layer.
+ */
+package com.example.udemy.sfgtdd.springtesting.petclinic.model;
+
---
+++ src/main/java/com/example/udemy/sfgtdd/springtesting/petclinic/repository/jpa/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The classes in this package represent the JPA implementation
+ * of PetClinic's persistence layer.
+ */
+package com.example.udemy.sfgtdd.springtesting.petclinic.repository.jpa;
+
