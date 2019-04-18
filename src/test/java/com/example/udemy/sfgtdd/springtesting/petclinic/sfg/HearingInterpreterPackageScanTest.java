87a04abe779440a34bb8b2bb5f1c54431a74621b
SergeA <ronnieo@gmail.com>
2019-04-17 22:12:06 -0400

    Added Sample Test with @ComponentScan Annotation

---
+++ src/test/java/com/example/udemy/sfgtdd/springtesting/petclinic/sfg/HearingInterpreterPackageScanTest.java
@@ -0,0 +1,29 @@
+package com.example.udemy.sfgtdd.springtesting.petclinic.sfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = HearingInterpreterPackageScanTest.TestConfig.class)
+class HearingInterpreterPackageScanTest {
+
+    @Configuration
+    @ComponentScan(basePackages = "com.example.udemy.sfgtdd.springtesting.petclinic.sfg")
+    static class TestConfig {
+
+    }
+
+    @Autowired
+    private HearingInterpreter hearingInterpreter;
+
+
+    @Test
+    void whatIheard() {
+        String word = hearingInterpreter.whatIheard();
+        assertEquals("Laurel", word);
+    }
+}
