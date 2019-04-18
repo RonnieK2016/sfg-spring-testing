8c14666acad07af7e1debb683c1ba9f1df6633fd
SergeA <ronnieo@gmail.com>
2019-04-17 22:00:16 -0400

    Updated Tests for hearingInterpreter

--- src/test/java/com/example/udemy/sfgtdd/springtesting/petclinic/sfg/HearingYannyTest.java
+++ src/test/java/com/example/udemy/sfgtdd/springtesting/petclinic/sfg/HearingYannyTest.java
@@ -1,15 +1,12 @@
 package com.example.udemy.sfgtdd.springtesting.petclinic.sfg;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {BaseConfig.class, YannyConfig.class})
+@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class})
 public class HearingYannyTest {
 
     @Autowired
