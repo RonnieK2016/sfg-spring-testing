3b616b888aa140cfad055e62358cd0dbe8d486cd
SergeA <ronnieo@gmail.com>
2019-04-18 12:17:22 -0400

    Completed Tests for OwnerController

--- src/test/java/com/example/udemy/sfgtdd/springtesting/petclinic/web/OwnerControllerTest.java
+++ src/test/java/com/example/udemy/sfgtdd/springtesting/petclinic/web/OwnerControllerTest.java
@@ -7,19 +7,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
 
+import static com.example.udemy.sfgtdd.springtesting.petclinic.web.OwnerController.VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.reset;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
@@ -48,7 +50,7 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners/new"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("owner"))
-                .andExpect(view().name("owners/createOrUpdateOwnerForm"));
+                .andExpect(view().name(VIEWS_OWNER_CREATE_OR_UPDATE_FORM));
     }
 
     @Test
@@ -98,5 +100,84 @@ class OwnerControllerTest {
                 .andExpect(view().name("redirect:/owners/" + owner1.getId()));
 
         then(clinicService).should().findOwnerByLastName(anyString());
+    }
+
+    @Test
+    void testNewOwnerPostValid() throws Exception {
+        mockMvc.perform(post("/owners/new")
+                .param("firstName", "Jimmy")
+                .param("lastName", "Buffett")
+                .param("address", "123 Duval St ")
+                .param("city", "Key West")
+                .param("telephone", "3151231234"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void testNewOwnerPostNotValid() throws Exception {
+        mockMvc.perform(post("/owners/new")
+                .param("firstName", "Jimmy")
+                .param("lastName", "Buffett")
+                .param("city", "Key West"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasErrors("owner"))
+                .andExpect(model().attributeHasFieldErrors("owner", "address"))
+                .andExpect(model().attributeHasFieldErrors("owner", "telephone"))
+                .andExpect(view().name(VIEWS_OWNER_CREATE_OR_UPDATE_FORM));
+    }
+
+    @Test
+    void testUpdateOwnerPostValid() throws Exception {
+        mockMvc.perform(post("/owners/{ownerid}/edit",1)
+                .param("firstName", "Jimmy")
+                .param("lastName", "Buffett")
+                .param("address", "123 Duval St ")
+                .param("city", "Key West")
+                .param("telephone", "3151231234"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    void testEditOwnerPostNotValid() throws Exception {
+        mockMvc.perform(post("/owners/{ownerid}/edit",1)
+                .param("address", "123 Duval St ")
+                .param("city", "Key West")
+                .param("telephone", "3151231234"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasErrors("owner"))
+                .andExpect(model().attributeHasFieldErrors("owner","firstName", "lastName"))
+                .andExpect(view().name(VIEWS_OWNER_CREATE_OR_UPDATE_FORM));
+    }
+    
+    @Test
+    void testInitUpdateForm() throws Exception {
+
+        given(clinicService.findOwnerById(anyInt())).willReturn(new Owner());
+
+        mockMvc.perform(get("/owners/{ownerId}/edit", 1))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(view().name(VIEWS_OWNER_CREATE_OR_UPDATE_FORM));
+
+        then(clinicService).should().findOwnerById(anyInt());
+    }
+
+    @Test
+    void testInitFindForm() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(view().name("owners/findOwners"));
+    }
+
+    @Test
+    void testShowOwner() throws Exception {
+
+        given(clinicService.findOwnerById(anyInt())).willReturn(new Owner());
+
+        mockMvc.perform(get("/owners/{ownerId}", 1))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(view().name("owners/ownerDetails"));
     }
 }
